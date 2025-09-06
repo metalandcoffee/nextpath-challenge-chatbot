4534cf15348318015ff49da730ddbe7ade571628
Ebonie Butler <ebonie@metalandcoffee.com>
2025-09-06 09:11:57 -0400

    setup wss

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-import express from 'express'
+import express from 'express';
+import { WebSocketServer } from 'ws';
 
+// Start HTTP server and listen on port 3000.
 const app = express()
 const port = 3000
 
@@ -10,3 +12,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// Create WebSocket using same HTTP server.
+const wss = new WebSocketServer({ server: app });
+
+wss.on('connection', function connection(ws) {
+    console.log('A new client has connected.');
+  ws.on('error', console.error);
+
+  // Triggered when server receives message from client.
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+//   ws.send('something');
+});
