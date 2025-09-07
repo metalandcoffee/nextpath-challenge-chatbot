4517ee5697dcb103591838418a1513c4bed70b16
Ebonie Butler <ebonie@metalandcoffee.com>
2025-09-07 11:51:48 -0400

    finish must have features and update readme

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project Name
+
+Brief description of what your project does.
+
+## Getting Started
+
+Follow these steps to get the project running locally:
+
+### Prerequisites
+
+- Node.js (version managed by `.nvmrc`)
+- npm
+
+### Installation & Setup
+
+1. **Clone the repository**
+   ```bash
+   git clone git@github.com:metalandcoffee/nextpath-challenge-chatbot.git
+   cd nextpath-challenge-chatbot
+   ```
+
+2. **Use the correct Node version**
+   ```bash
+   nvm use
+   ```
+
+3. **Install dependencies**
+   ```bash
+   npm i
+   ```
+
+4. **Start the Node server**
+   ```bash
+   npm start
+   ```
+
+5. **Open the application**
+   - Open `index.html` in your browser
+   - Start developing!
+
+## Usage
+
+Add information about how to use your project here.
+
+## Contributing
+
+Guidelines for contributing to the project.
+
+## License
+
+Your license information here.
--- server.js
+++ server.js
@@ -5,6 +5,28 @@ import { readFile, writeFile } from 'node:fs';
 // Start app.
 const app = express()
 const port = 3000
+let chatHistory;
+let botMessages;
+
+// Read in chat history and send to client.
+readFile('log.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log('Log file does not exist. Creating...');
+        writeFile('log.txt', '', (err) => {
+            if (err) throw err;
+        });
+    }
+    chatHistory = !data ? [] : data.split('\n');
+}); 
+
+// Queue up bot acknowledgement messages.
+readFile('automatedMessages.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log('Log file does not exist.');
+        return;
+    }
+    botMessages = data === '' ? [] : data.split('\n');
+}); 
 
 app.get('/', (req, res) => {
     res.send('Hello World! It\'s Metal & Coffee!')
@@ -19,23 +41,33 @@ const server = app.listen(port, () => {
 // Create WebSocket using same HTTP server.
 const wss = new WebSocketServer({ server });
 
+// Get random number.
+const getRandomBotMsg = () => {
+  return botMessages[Math.floor(Math.random() * 15)];
+}
+
+// Watch for all connections on WSS.
 wss.on('connection', function connection(ws) {
     console.log('A new client has connected.');
-    // Read in chat history and send to client.
-    readFile('log.txt', (err, data) => {
-        if (err) {
-            console.log('Log file does not exist.');
-            return;
-        }
-        console.log(data);
-    }); 
+    if (chatHistory) ws.send(JSON.stringify(chatHistory));
     ws.on('error', console.error);
 
-    // Triggered when server receives message from client.
+    // When connection receives message from client.
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
-        // @todo read in file. append with timestamp. write to file.
-    });
+        const message = data.toString();
+        const botMessage = {user: 'bot', text: getRandomBotMsg() };
 
-//   ws.send('Acknowledged!');
+        // Update chat logs in memory.
+        chatHistory.push(message);
+        chatHistory.push(JSON.stringify(botMessage));
+
+        // Update log file.
+        writeFile('log.txt', chatHistory.join('\n'), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+
+        // Acknowledge message.
+        ws.send(JSON.stringify(botMessage));
+    });
 });
